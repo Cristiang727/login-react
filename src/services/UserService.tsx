53ad0b627193c6bf1d9d7e8b8a8bd96382ea3b9d
Cristiang727 <criatiangarcia727@gmail.com>
2025-10-03 10:55:39 -0500

    login completo

--- src/services/UserService.tsx
+++ src/services/UserService.tsx
@@ -3,5 +3,5 @@ import type { User } from "../types/UserType";
 
 export const getMe = async ():Promise<User> =>{
     const {data} = await Conexion.get("/me");
-    return data;
+    return data.user;
 }
